1a20d1e7166c1f301cccea158e3bc4fe43217a4a
Carleigh Crockett <capleugh@gmail.com>
2020-02-10 10:33:01 -0700

    Update faraday connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,16 @@
 class SearchController < ApplicationController
   def index
-    house = params[:houseId]
     conn = Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
       f.adapter Faraday.default_adapter
     end
 
-    response = conn.get("/houses/#{house}?key=#{ENV["HARRY_POTTER_API_KEY"]}")
-    require "pry"; binding.pry
+    response = conn.get("characters?key=#{ENV["HARRY_POTTER_API_KEY"]}")
 
     json = JSON.parse(response.body, symbolize_names: true)
 
-
+    @members = json.find_all do |member|
+      member[:orderOfThePhoenix] == true && member[:house] == "Gryffindor"
+    end
+    # require "pry"; binding.pry
   end
 end
