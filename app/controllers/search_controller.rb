5cb87bb9e6eaaa98d131617404e28d623ffe5d52
Carleigh Crockett <capleugh@gmail.com>
2020-02-10 10:50:31 -0700

    Begin controller refactor and git rid of map

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,17 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
-      f.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("characters?key=#{ENV["HARRY_POTTER_API_KEY"]}")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    @members = json.find_all do |member|
-      member[:orderOfThePhoenix] == true && member[:house] == "Gryffindor"
-    end
-    # require "pry"; binding.pry
+    render locals: {
+      members: MemberSearch.new
+      
+    }
+    # conn = Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
+    #   f.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("characters?house=Gryffindor&orderOfThePhoenix=true&key=#{ENV["HARRY_POTTER_API_KEY"]}")
+    #
+    # json = JSON.parse(response.body, symbolize_names: true)
+    #
+    # @members = json
   end
 end
