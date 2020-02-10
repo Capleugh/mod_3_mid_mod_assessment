debb233fe277cf6047a5244fe1461805f7e35610
Carleigh Crockett <capleugh@gmail.com>
2020-02-10 10:33:51 -0700

    Update test for additional css about members

--- spec/features/user_can_search_memebers_of_gryffindor_spec.rb
+++ spec/features/user_can_search_memebers_of_gryffindor_spec.rb
@@ -11,13 +11,16 @@ describe 'As a user' do
 
       expect(current_path).to eq(search_path)
 
+      expect(page).to have_content('Order of the Phoenix')
       expect(page).to have_content('21 for Gryffindor')
-      expect(page).to have_content('Order of the Phoenix members')
 
       expect(page).to have_css('.members', count: 21)
 
       within(first('.members')) do
         expect(page).to have_css('.name')
+        expect(page).to have_css('.role')
+        expect(page).to have_css('.house')
+        expect(page).to have_css('.patronus')
       end
     end
   end
